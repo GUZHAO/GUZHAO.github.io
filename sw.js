d67a7b496ef6d4472a714a0694f8c7826f527905
GUZHAO <guzhao1990@gmail.com>
2018-11-03 22:39:36 -0400

    fix: fix service worker file
    Description: add to home screen is not showing up and it seems to be site.js problem
    
    Status: in progress - remove some parts for testing

--- sw.js
+++ sw.js
@@ -15,19 +15,19 @@ self.addEventListener('fetch', function (event) {
 //     );
 // });
 
-self.addEventListener('fetch', function (event) {
-    if (event.request.url === 'https://guzhao.github.io./') {
-        console.info('responding to github site fetch with Service Worker! 🤓');
-        event.respondWith(fetch(event.request).catch(function (e) {
-            let out = {Gold: 1, Size: -1, Actions: []};
-            return new Response(JSON.stringify(out));
-        }));
-        return;
-    }
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+// self.addEventListener('fetch', function (event) {
+//     if (event.request.url === 'https://guzhao.github.io./') {
+//         console.info('responding to github site fetch with Service Worker! 🤓');
+//         event.respondWith(fetch(event.request).catch(function (e) {
+//             let out = {Gold: 1, Size: -1, Actions: []};
+//             return new Response(JSON.stringify(out));
+//         }));
+//         return;
+//     }
+//
+//     event.respondWith(
+//         caches.match(event.request).then(function (response) {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
